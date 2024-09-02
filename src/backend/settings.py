c161f59f86d4bde29d95bba526785ddc1bea1e45
shoham <shoamb5555@gmail.com>
2024-09-02 15:07:09 +0300

    still not working but im scared

--- src/backend/settings.py
+++ src/backend/settings.py
@@ -3,11 +3,11 @@ port = "5432"
 # database = os.environ['DB_NAME']
 # username = os.environ['DB_USERNAME']
 # password = os.environ['DB_PASSWORD']
+# host = os.environ['DB_HOST']
 database = "files_db"
 password = "Asafba123"
 username = "hello"
-host = "postgres_db"
-
+host = "postgres-db"
 connection_string = (
     f"postgres://{username}:{password}@{host}:{port}/{database}")
 
--- src/frontend/settings.py
+++ src/frontend/settings.py
@@ -1,16 +1,13 @@
 import os
-print(os.environ)
 port = "5432"
-# database = os.environ['DB_NAME']
-# username = os.environ['DB_USERNAME']
-# password = os.environ['DB_PASSWORD']
-database = "files_db"
-password = "Asafba123"
-username = "hello"
-host = "postgres_db"
-
-# Connection string
+database = os.environ['DB_NAME']
+username = os.environ['DB_USERNAME']
+password = os.environ['DB_PASSWORD']
+host = os.environ['DB_HOST']
+# database = "files_db"
+# password = "Asafba123"
+# username = "hello"
+# host = "postgres-db"
 
 connection_string = (
-    f"postgres://{username}:{password}@{host}:{port}/{database}"
-)
+    f"postgres://{username}:{password}@{host}:{port}/{database}")
