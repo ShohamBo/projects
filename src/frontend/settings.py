9fe7ad70cdc95091fdc8e29ec326bfa689cde2c1
shoham <shoamb5555@gmail.com>
2024-09-02 10:06:11 +0300

    fixed image not loading

--- src/frontend/settings.py
+++ src/frontend/settings.py
@@ -1,9 +1,12 @@
 import os
 print(os.environ)
 port = "5432"
-database = os.environ['DB_NAME']
-username = os.environ['DB_USERNAME']
-password = os.environ['DB_PASSWORD']
+# database = os.environ['DB_NAME']
+# username = os.environ['DB_USERNAME']
+# password = os.environ['DB_PASSWORD']
+database = "files_db"
+password = "Asafba123"
+username = "hello"
 host = "postgres_db"
 
 # Connection string
