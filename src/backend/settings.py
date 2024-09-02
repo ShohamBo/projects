52afc54402c9702931597139eddd2f4cff8fb248
shoham <shoamb5555@gmail.com>
2024-09-02 16:47:03 +0300

    a problem with the db connection to the rest of the nodes

--- src/backend/settings.py
+++ src/backend/settings.py
@@ -1,13 +1,13 @@
 import os
-port = "5432"
 # database = os.environ['DB_NAME']
 # username = os.environ['DB_USERNAME']
 # password = os.environ['DB_PASSWORD']
 # host = os.environ['DB_HOST']
+port = "5432"
 database = "files_db"
 password = "Asafba123"
 username = "hello"
-host = "postgres-db"
+host = "postgresdb"
 connection_string = (
     f"postgres://{username}:{password}@{host}:{port}/{database}")
 
--- src/frontend/settings.py
+++ src/frontend/settings.py
@@ -1,13 +1,13 @@
 import os
 port = "5432"
-database = os.environ['DB_NAME']
-username = os.environ['DB_USERNAME']
-password = os.environ['DB_PASSWORD']
-host = os.environ['DB_HOST']
-# database = "files_db"
-# password = "Asafba123"
-# username = "hello"
-# host = "postgres-db"
-
+# database = os.environ['DB_NAME']
+# username = os.environ['DB_USERNAME']
+# password = os.environ['DB_PASSWORD']
+# host = os.environ['DB_HOST']
+database = "files_db"
+password = "Asafba123"
+username = "hello"
+host = "postgresdb"
 connection_string = (
     f"postgres://{username}:{password}@{host}:{port}/{database}")
+
